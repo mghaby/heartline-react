f84766788595fefcb9adbbe51830ae114d64e360
mghaby <mark.ghaby71@hotmail.com>
2021-01-30 01:03:34 +1100

    react components updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Landing from './Landing';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Nav />
+      <Landing />
     </div>
   );
 }
--- src/Landing.js
+++ src/Landing.js
@@ -1,8 +1,11 @@
-
+import WeightLog from './WeightLog';
 
 function Landing() {
     return(
+        <div>
         <h1>Landing</h1>
+        <WeightLog />
+        </div>
     )
 }
 
