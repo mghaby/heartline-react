3e7279d984f712f861e797447377c514a35a9776
mghaby <mark.ghaby71@hotmail.com>
2021-02-03 16:55:20 +1100

    base form done with material ui, still needs spacing and further styling

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,65 +1,227 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
-// import { makeStyles } from '@material-ui/core/styles';
-// import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     '& > *': {
-//       margin: theme.spacing(1),
-//       width: '25ch',
-//     },
-//   },
-// }));
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+const genders = [
+  {
+    value: 5,
+    label: 'Male',
+  },
+  {
+    value: -161,
+    label: 'Female',
+  }
+];
+
+const activities = [
+  {
+    value: 1.2,
+    label: "Sedentary - Little or no excersise",
+  },
+  {
+    value: 1.375,
+    label: "Light - Exercise 1-3 times/week",
+  },
+  {
+    value: 1.465,
+    label: "Moderate - Exercise or intense 4-5 times/week",
+  },
+  {
+    value: 1.55,
+    label: "Active - Daily exercise or intense exercise 3-4 times/week",
+  },
+  {
+    value: 1.725,
+    label: "Very Active - Intense exercise 6-7 times/week",
+  },
+  {
+    value: 1.9,
+    label: "Extra Active - Very intense exercise daily, or phyiscal job",
+  },
+  {
+    value: 1.95,
+    label: "Athlete",
+  }
+]
+
+const socials = [
+  {
+    value: true,
+    label: "Yes",
+  },
+  {
+    value: false,
+    label: "No",
+  },
+]
+
 
 function SignUp() {
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
-  // const classes = useStyles();
+  const classes = useStyles();
+  const [gender, setGender] = React.useState('male');
+  const [activity, setActivity] = React.useState('Sedentary')
+  const [social, setSocial] = React.useState('Yes')
+
+  const handleChange = (event) => {
+    setGender(event.target.value);
+  };
   
+  const handleChangeActivity = (event) => {
+    setActivity(event.target.value);
+  };
+
+  const handleChangeSocial = (event) => {
+    setSocial(event.target.value);
+  };
+
   return (
       <div>
-         {/* <TextField id="standard-basic" label="Standard" /> */}
         <p>Our system uses an algorithim to calculate YOUR needs for YOUR goals! Please input your details below in the metric system.</p>
     <form>
-      <input type="text" placeholder="Username" name="Username" ref={register({required: true, maxLength: 80})} />
+    <TextField
+          required
+          id="outlined"
+          label="Username"
+          variant="outlined"
+        />
       <br/>
-      <input type="text" placeholder="Password" name="Password" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
+      <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+          />
       <br/>
-      <input type="text" placeholder="Password Confirmation" name="PasswordConfirmation" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
-      <br/>
-      <input type="number" placeholder="Goal Weight" name="GoalWeight" ref={register({required: true, min: 0})} />
-      <br/>
-      <input type="number" placeholder="Weight" name="Weight" ref={register({required: true, min: 0})} />
-      <br/>
-      <input type="number" placeholder="Age" name="Age" ref={register({required: true, min: 0})} />
-      <br/>
-      <input type="number" placeholder="Goal Water Level" name="GoalWaterLevel" ref={register({min: 2})} />
-      <br/>
-      <select name="Gender" ref={register}>
-        <option value="Male">Male</option>
-        <option value="Female">Female</option>
-      </select>
-      <br/>
-      <select name="Daily Activity Level" ref={register}>
-        <option value="Sedentary - Little to no exercise">Sedentary - Little to no exercise</option>
-        <option value="Light - Exercise 1-3 times a week">Light - Exercise 1-3 times a week</option>
-        <option value="Moderate - Exercise or intense exercise 4-5 times a week">Moderate - Exercise or intense exercise 4-5 times a week</option>
-        <option value="Active - Daily exercise or intense exercise 4-5 times a week">Active - Daily exercise or intense exercise 4-5 times a week</option>
-        <option value="Very Active - Intense exercise 6-7 times a week">Very Active - Intense exercise 6-7 times a week</option>
-        <option value=" Extra Active - Very intense exercise daily, or physical job"> Extra Active - Very intense exercise daily, or physical job</option>
-        <option value="Athlete">Athlete</option>
-      </select>
-      <br/>
-      <p>Would you like your progress to be seen by other users?</p>
-      <select name="Would you like your progress to be seen by other users?" ref={register}>
-        <option value="Yes">Yes</option>
-        <option value="No">No</option>
-      </select>
+      <TextField
+          id="outlined-password-input"
+          label="Password Confirmation"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+          />
       <br/>
 
-      <input type="submit" onSubmit={handleSubmit(onSubmit)}/>
+      <TextField
+          id="outlined-number"
+          label="Current Weight"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+        />
+        <br/>
+        <TextField
+          id="outlined-number"
+          label="Goal Weight"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+        />
+        <br/>
+        <TextField
+          id="outlined-number"
+          label="Age"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+        />
+        <br/>
+        <TextField
+          id="outlined-number"
+          label="Goal Water"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+        />
+        <br/>
+
+        <span>
+        <TextField
+          id="outlined-select-currency"
+          select
+          label="Select Gender"
+          value={gender}
+          onChange={handleChange}
+          helperText="Please select your Gender"
+          variant="outlined"
+        >
+          {genders.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      <br/>
+   </span>
+
+   <div>
+     <TextField
+     id="outlined-select-currency"
+     select
+     label="Select Activity Level"
+     value={activity}
+     onChange={handleChangeActivity}
+     helperText="Please Select your Daily Activity Level"
+     varient="outlined"
+     >
+       {activities.map((option) => (
+         <MenuItem key={option.value} value={option.value}>
+           {option.label}
+         </MenuItem>
+       ))}
+       </TextField>
+       <br/>
+   </div>
+
+    <div>
+   <TextField
+     id="outlined-select-currency"
+     select
+     label="Select Public Setting"
+     value={social}
+     onChange={handleChangeSocial}
+     helperText="Would You Like Your Progress Made Public?"
+     varient="outlined"
+     >
+       {socials.map((option) => (
+         <MenuItem key={option.value} value={option.value}>
+           {option.label}
+         </MenuItem>
+       ))}
+       </TextField>
+       <br/>
+   </div>
+
+        <div>
+          <Button variant="contained" size="large" color="primary" className={classes.margin}>
+          Submit</Button>
+        </div>
     </form>
     </div>
   );
