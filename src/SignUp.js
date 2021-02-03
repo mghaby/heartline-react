4eee2d21d917724b654111e609c39316b47fd118
mghaby <mark.ghaby71@hotmail.com>
2021-02-03 13:29:44 +1100

    some styling done and material ui set on sign up form

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+// import { makeStyles } from '@material-ui/core/styles';
+// import TextField from '@material-ui/core/TextField';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     '& > *': {
+//       margin: theme.spacing(1),
+//       width: '25ch',
+//     },
+//   },
+// }));
 
 function SignUp() {
   const { register, handleSubmit, errors } = useForm();
   const onSubmit = data => console.log(data);
   console.log(errors);
+  // const classes = useStyles();
   
   return (
       <div>
+         {/* <TextField id="standard-basic" label="Standard" /> */}
+        <p>Our system uses an algorithim to calculate YOUR needs for YOUR goals! Please input your details below in the metric system.</p>
     <form>
       <input type="text" placeholder="Username" name="Username" ref={register({required: true, maxLength: 80})} />
       <br/>
       <input type="text" placeholder="Password" name="Password" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
       <br/>
-      <input type="number" placeholder="Goal Weight" name="Goal Weight" ref={register({required: true, min: 0})} />
+      <input type="text" placeholder="Password Confirmation" name="PasswordConfirmation" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
+      <br/>
+      <input type="number" placeholder="Goal Weight" name="GoalWeight" ref={register({required: true, min: 0})} />
       <br/>
       <input type="number" placeholder="Weight" name="Weight" ref={register({required: true, min: 0})} />
       <br/>
       <input type="number" placeholder="Age" name="Age" ref={register({required: true, min: 0})} />
       <br/>
-      <input type="number" placeholder="Goal Water Level" name="Goal Water Level" ref={register({min: 2})} />
+      <input type="number" placeholder="Goal Water Level" name="GoalWaterLevel" ref={register({min: 2})} />
       <br/>
       <select name="Gender" ref={register}>
         <option value="Male">Male</option>
@@ -36,6 +52,7 @@ function SignUp() {
         <option value="Athlete">Athlete</option>
       </select>
       <br/>
+      <p>Would you like your progress to be seen by other users?</p>
       <select name="Would you like your progress to be seen by other users?" ref={register}>
         <option value="Yes">Yes</option>
         <option value="No">No</option>
