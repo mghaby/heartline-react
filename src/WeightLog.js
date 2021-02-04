96cf861b2ecafd7768304b5e264b73a220e29073
mghaby <mark.ghaby71@hotmail.com>
2021-02-04 12:38:51 +1100

    public weight log basic styling done

--- src/WeightLog.js
+++ src/WeightLog.js
@@ -1,9 +1,77 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
 
 function WeightLog(){
+    const classes = useStyles();
     return(
-        <h1>WeightLog</h1>
+        <div>
+        <Grid
+            container
+            direction="row"
+            justify="space-evenly"
+            alignItems="center">
+            <Paper>
+                Username:
+                <br/>
+                Goal Weight:
+                <br/>
+                Current Weight:
+                <br/>
+                Age:
+            </Paper>
+            <Paper>
+                Username:
+                <br/>
+                Goal Weight:
+                <br/>
+                Current Weight:
+                <br/>
+                Age:
+            </Paper>
+            <Paper>
+                Username:
+                <br/>
+                Goal Weight:
+                <br/>
+                Current Weight:
+                <br/>
+                Age:
+            </Paper>
+            <Paper>
+                Username:
+                <br/>
+                Goal Weight:
+                <br/>
+                Current Weight:
+                <br/>
+                Age:
+            </Paper>
+            <Paper>
+                Username:
+                <br/>
+                Goal Weight:
+                <br/>
+                Current Weight:
+                <br/>
+                Age:
+            </Paper>
+        </Grid>
+        <hr/>
+        </div>
     )
-}
+};
 
 export default WeightLog;
