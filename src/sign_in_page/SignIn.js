b527c09e6a25f6f2ffe41b7752e02e8ee11f5574
nguyenmarc7 <nguyenmarc7@gmail.com>
2021-02-05 21:42:19 +1100

    quick and dirty sigin refactor

--- src/sign_in_page/SignIn.js
+++ src/sign_in_page/SignIn.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -26,8 +26,9 @@ const users = [
 
 function SignIn() {
   const classes = useStyles();
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  // const [username, setUsername] = useState('')
+  // const [password, setPassword] = useState('')
+    const [formData, setFormData] = useState({username: '', password: ''})
 
   // onFormSubmit = (event) => {
   //   event.preventDefault()
@@ -53,12 +54,16 @@ function SignIn() {
   //     })
   //   }
   // }
-
+  // useEffect()
   // onInputChange = (event) => {
   //   this.setState({
   //     [event.target.id]: event.target.value
   //   })
   // }
+    const handleSubmit = () => {
+      console.log(formData)
+    }
+
 
     return (
     // <div className="container">
@@ -73,6 +78,7 @@ function SignIn() {
     //     <input type="submit" value="Submit" id="submit" />
     //   </form>
     // </div>
+    // onSubmit={handleSubmit}
     <form className={classes.root} noValidate autoComplete="off">
       <Grid
       container
@@ -83,7 +89,10 @@ function SignIn() {
       <TextField 
       id="outlined-username" 
       label="Username" 
-      variant="outlined" />
+      variant="outlined" 
+      name="username"
+      value={formData.username}
+      onChange={(e) => setFormData({...formData, username: e.target.value})}/>
      
      <br/>
 
@@ -93,12 +102,15 @@ function SignIn() {
           type="password"
           autoComplete="current-password"
           variant="outlined"
+          name="password"
+          value={formData.password}
+          onChange={(e) => setFormData({...formData, password: e.target.value})}
           />
           <br/>
-        <Button variant="contained" size="large" color="primary" className={classes.margin}>
+        <Button variant="contained" size="large" color="primary" className={classes.margin} onClick={handleSubmit}>
         Submit</Button>
         </Grid>
-    </form>
+    </form >
   )
 }
 
