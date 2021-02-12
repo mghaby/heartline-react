ba45922971ab0c1d764df3f101d17d54cca44acc
mghaby <mark.ghaby71@hotmail.com>
2021-02-12 15:57:05 +1100

    baseurl changed

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const heartLineApi = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://heartline-api.herokuapp.com'
 })
 
 heartLineApi.interceptors.request.use((req) => {
